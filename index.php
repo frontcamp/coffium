066346a231a8a8f51691b014d66b9e97068c4c58
Maksym Plaksin <maxim.plaksin@gmail.com>
2025-11-18 15:57:04 +0100

    improve finalization logic (allow add recursive finalization functions)

--- index.php
+++ index.php
@@ -4,31 +4,38 @@ $GLOBALS['SYS']['included'][] = __FILE__;
 $GLOBALS['SYS']['runtime'] = $_SERVER['REQUEST_TIME_FLOAT'] ?? microtime(true);
 
 $_FINALIZERS = array();
+function register_finalizer(callable $func_name, int $order=0)
+{
+    global $_FINALIZERS;
+    $_FINALIZERS[$order][] = $func_name;
+}
 
+# initialization
 require('config-global.php');
 require('config-server.php');
 require('config-custom.php');
 
+# include core libraries
 require('libs/inc.common.php');
 require('libs/inc.registry.php');
 require('libs/inc.request.php');
 require('libs/inc.response.php');
 
+# run router
 core_use_handler(sys_get('route.path'));
 
-function register_finalizer(callable $func_name)
+# run finalizers (registration during execution is allowed)
+while (!empty($_FINALIZERS))
 {
-    global $_FINALIZERS;
-    array_push($_FINALIZERS, $func_name);
+    ksort($_FINALIZERS);
+    $group = array_key_first($_FINALIZERS);
+    while (!empty($_FINALIZERS[$group]))
+    {
+        $finalizer = array_shift($_FINALIZERS[$group]);
+        $finalizer();
+    }
+    unset($_FINALIZERS[$group]);
 }
-
-foreach ($_FINALIZERS as $finalizer)
-{
-    if (is_callable($finalizer)) $finalizer();
-}
-
-#
-# Run helpers (debug)
 
 if (IS_VIP) require('libs/inc.helpers.php');
 
